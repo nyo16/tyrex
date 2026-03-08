bda3c63eae3b7b39e5a80c1ba9a6df350ca776d0
Niko Maroulis <niko@hackthebox.eu>
2026-03-08 18:58:11 -0400

    Add precompiled NIF checksums for v0.1.0

---
+++ checksum-Elixir.Tyrex.Native.exs
@@ -0,0 +1,7 @@
+%{
+  "libtyrex-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:eadc04daa29f9a21e3189a10452ace806ba08ce4ca1422d74a5664a9cbf4fecc",
+  "libtyrex-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:34fe4cf9631f65babf6d7737e7c73ed72e0ccd8ab0ea38eb44d22b95a2cd4f3f",
+  "libtyrex-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:f35db1a675a2e68c873016d2a97350f29e154f596aea63ae3fcf3096b2f53da9",
+  "libtyrex-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:e55828c55211d754a9cb5e90d0c59b5cb499dadbb980337ca054319bea4f6197",
+  "tyrex-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:df38e87b1c874bc71424269772c67f07fb82ffcb237d458009fb8f7c4eabc46f",
+}
