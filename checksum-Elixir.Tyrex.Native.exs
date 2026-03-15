378c175fc142cd4181fd2edfb5c22f46662a67b5
Niko Maroulis <nik.maroulis@gmail.com>
2026-03-14 21:50:07 -0400

    Add precompiled NIF checksums for v0.2.0

--- checksum-Elixir.Tyrex.Native.exs
+++ checksum-Elixir.Tyrex.Native.exs
@@ -1,1 +1,6 @@
-%{}
+%{
+  "libtyrex-v0.2.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:5078a5ea36580912eadc48898c6ae4fd226ed0ffacf2d29c4af5a4db763b4cfb",
+  "libtyrex-v0.2.0-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:ea92d13040c0e6c107732adf1799e8e90cc0aeca66b051832b7df00ead10fcf2",
+  "libtyrex-v0.2.0-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:c8f2d562f1f838740c10842f3ca9169ba3e1921e122f4e08e060f9334ce959e0",
+  "libtyrex-v0.2.0-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:2f3c7d5ae54c21043b93103bb44bd0f738e05241db57a6175c92dacbd47db16d",
+}
