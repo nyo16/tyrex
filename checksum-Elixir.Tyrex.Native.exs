6278aa8b0fdbbb25ab47910a40eb2ef9a24bfb44
Niko Maroulis <nik.maroulis@gmail.com>
2026-05-24 13:31:10 -0400

    Update precompiled NIF checksums for v0.3.0

--- checksum-Elixir.Tyrex.Native.exs
+++ checksum-Elixir.Tyrex.Native.exs
@@ -1,6 +1,6 @@
 %{
-  "libtyrex-v0.2.1-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:cdd6ea144b4f9bb56cc960f55cd9c7cde508cf27e400170bb08bb422d9f54ce0",
-  "libtyrex-v0.2.1-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:c48160bf9b75ee1675eb1a4642046b4e4d5fd00f1044cade639844fbd2322e5e",
-  "libtyrex-v0.2.1-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:7754fc5b5ef360095873e82b1488630b9b273150b488575e7d126552693c4dad",
-  "libtyrex-v0.2.1-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:0684bb2ca702bc7ef6b1181055d27012e30e3dae0a377cf50418c1a07fb73b4b",
+  "libtyrex-v0.3.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:e7b99d17e1c1e2309266128871715c897173539c56e178af0dcf1179dd68201f",
+  "libtyrex-v0.3.0-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:47ad45ae48bc61f2758f197bb69b1bb89ff673d2b1e87b66561a82b25c1abb68",
+  "libtyrex-v0.3.0-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:263e7ca7fe3a7a1d4ec68e306a97e58b165fd7e07fb5a78b009f591993edd976",
+  "libtyrex-v0.3.0-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:7635c4efd15fd158c28b618dad3d2488a1cc4a1464c3bddd663ed7185e0c8fb8",
 }
